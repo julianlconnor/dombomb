5df5f6b4f23845fdd1bf2a38a0a75239c7c86d1d
Danny Cosson <dcosson@gmail.com>
2012-09-29 16:08:23 -0500

    updated bomb page to use settings

--- flask/model/Bomb.py
+++ flask/model/Bomb.py
@@ -1,6 +1,9 @@
 import os
 import datetime
 
+import settings
+
+
 class DomBombMongo():
     ##
     ## Database and collection names
@@ -13,7 +16,6 @@ class DomBombMongo():
     @classmethod
     def setup_mongo_indexes(klass):
         from pymongo import DESCENDING
-
         db = klass.mdbc()
         db.ensure_index([(klass.A_IDENTIFIER, DESCENDING) ])
 
@@ -21,42 +23,33 @@ class DomBombMongo():
     def mdbc(klass):
         """ Memoized pymongo connection.
         """
-        
         if not getattr(klass, 'MONGO_COLL_POINTER', None):
             connection = klass.mongo_connection()
             db = connection[klass.MONGO_DB_NAME]
             klass.MONGO_COLL_POINTER = db[klass.MONGO_COLLECTION_NAME]
-
         return klass.MONGO_COLL_POINTER
 
     @classmethod
     def mongo_connection(klass):
         """ returns a pointer to the DB"""
-
         if not getattr(klass, 'MONGO_CONNECTION', None):
-
             from pymongo.connection import Connection
-            connection = Connection(klass.MONGO_HOST, klass.MONGO_PORT)
+            connection = Connection(settings.MONGO_HOST, settings.MONGO_PORT)
             klass.MONGO_CONNECTION = connection
-
         return klass.MONGO_CONNECTION
+
 
 class Bomb(DomBombMongo):
     ##
     ## Attributes
     ##
     A_OBJECT_ID = "_id"
-
     A_X = 'x'
     A_Y = 'y'
-
     A_WIDTH      = 'width'
     A_HEIGHT     = 'height'
-
     A_DEFAULT_DIMENSION = '50'
-
     A_IDENTIFIER = 'identifier'
-
     A_CREATED_AT = 'created_at'
     A_UPDATED_AT = 'updated_at'
 
@@ -70,12 +63,10 @@ class Bomb(DomBombMongo):
             klass.A_WIDTH  : kwargs.get(klass.A_WIDTH, klass.A_DEFAULT_DIMENSION),
             klass.A_HEIGHT : kwargs.get(klass.A_HEIGHT, klass.A_DEFAULT_DIMENSION),
             klass.A_CREATED_AT : datetime.datetime.now(),
-            klass.A_UPDATED_AT : datetime.datetime.now()
+            klass.A_UPDATED_AT : datetime.datetime.now(),
         }
-
         klass.mdbc().insert(doc)
         id = str(doc.get(klass.A_OBJECT_ID))
-
         return id
 
     @classmethod
@@ -83,10 +74,7 @@ class Bomb(DomBombMongo):
         """ Returns the results for the provided identifier.
         """
         db = klass.mdbc()
-
         spec = {
             klass.A_IDENTIFIER : kwargs.get(klass.A_IDENTIFIER, None)
         }
-
         return list(db.find(spec))
-
